4dd294f1c3d1c6d55ca477f739ea9515e1642167
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-09-18 22:49:11 +0530

    added cors

--- travelDiaries/app.js
+++ travelDiaries/app.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routing/user-routes";
 import postRouter from "./routing/post-routes";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 
 // middlewares
+app.use(cors());
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/posts", postRouter);
